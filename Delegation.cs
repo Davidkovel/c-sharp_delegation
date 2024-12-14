a3e343797c804d8a474a1ef8892721fc3af95fd6
David <113856260+Davidkovel@users.noreply.github.com>
2024-12-14 12:48:11 +0000

    lab task 1 is done

---
+++ Delegation.cs
@@ -0,0 +1,22 @@
+﻿namespace DelegateClass;
+
+public class MessageHandler
+{
+
+    public delegate void MessageDelegate(string message);
+
+    public static void ShowMessage(string message)
+    {
+        Console.WriteLine($"Message: {message}");
+    }
+
+    public static void ShowUpperMessage(string message)
+    {
+        Console.WriteLine($"Uppercase Message: {message.ToUpper()}");
+    }
+
+    public static void ShowLowerMessage(string message)
+    {
+        Console.WriteLine($"Lowercase Message: {message.ToLower()}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DelegateClass;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                MessageHandler.MessageDelegate messageDelegate;
+
+                messageDelegate = MessageHandler.ShowMessage;
+                messageDelegate("Hello, World!");
+
+                messageDelegate = MessageHandler.ShowUpperMessage;
+                messageDelegate("Hello, World!");
+
+                messageDelegate = MessageHandler.ShowLowerMessage;
+                messageDelegate("Hello, World!");
+
+                Console.WriteLine("All methods executed successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Program execution completed.");
+            }
+        }
+    }
+}
+
+
+/*
+Message: Hello, World!
+Uppercase Message: HELLO, WORLD!
+Lowercase Message: hello, world!
+All methods executed successfully.
+Program execution completed.
+
+C:\Users\David\source\repos\ConsoleApp1\ConsoleApp1\bin\Debug\net8.0\ConsoleApp1.exe (process 4816) exited with code 0 (0x0).
+Press any key to close this window . . .
+
+ 
+*/
